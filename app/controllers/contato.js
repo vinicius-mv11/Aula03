e235befa1e2783df3f415d84eebc97213ad6328c
vinicius-mv11 <88984568+vinicius-mv11@users.noreply.github.com>
2023-02-24 23:36:30 +0000

    Initial commit

---
+++ app/controllers/contato.js
@@ -0,0 +1,22 @@
+var contatos = [
+  { _id: 1, nome: 'Fabio Teixeira', email: 'fabio.teixeira@ifsp.edu.br' },
+  { _id: 2, nome: 'Fabiano Teixeira', email: 'fabiano.teixeira@ifsp.edu.br' },
+  { _id: 3, nome: 'Melissa Teixeira', email: 'melissa.teixeira@ifsp.edu.br' }
+];
+
+module.exports = function() {
+  var controller = {};
+  controller.listaContatos = function(req, res) {
+    res.json(contatos);
+  };
+
+  controller.obtemContato = function(req, res) {
+    console.log('Seleciona contato: ' + req.params.id);
+    var idContato = req.params.id;
+    var contato = contatos.filter((contato) => {
+      return contato._id == idContato;
+    });
+    contato ? res.json(contato) : res.status(404).send('Contato não encontrado!');
+  };
+  return controller;
+};
